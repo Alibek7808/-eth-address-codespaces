4e04281db2745f82008ed4128d608983302e50d3
Alibek7808 <80057809+Alibek7808@users.noreply.github.com>
2025-07-12 12:37:49 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+set -e
+
+echo "📥 Cloning libsecp256k1..."
+git clone https://github.com/bitcoin-core/secp256k1 || true
+
+cd secp256k1
+./autogen.sh
+./configure --enable-module-ecdh --enable-experimental
+make
+cd ..
+
+echo "🔧 Compiling eth_address..."
+gcc main.c -o eth_address \
+  -Isecp256k1/include \
+  secp256k1/.libs/libsecp256k1.a \
+  -lcrypto
+
+echo "✅ Done! Run with ./eth_address"
 
